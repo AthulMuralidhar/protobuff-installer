8fc2c04667afaf4c255255fb10060ba19c105ba9
Athul Muralidhar <athul.muralidhar@print.com>
2023-08-24 17:44:41 +0200

    initial porject objectives

---
+++ cmd/main.go
@@ -0,0 +1,12 @@
+package main
+
+// GOALS
+// - make a cmd line interface to install protobuf and protobuf compiler
+// - so the program should have an interactive interface which asks for the protobuf version and the language and the arch etc, all the important stuff
+// - same with the protoc compiler, version arch full or partial etc
+// - if left empty, just used the latest and install
+// it should also install everything at the usr level without sudo perms unless explicitely mentioned
+
+func main() {
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AthulMuralidhar/protobuff-installer
+
+go 1.21.0
