9acfbecc95d141b840d33aae46866abdb2652980
Athul Muralidhar <athul.muralidhar@print.com>
2023-08-27 17:57:25 +0200

    ah fck my life, soo many fixmes and some stuff works

--- cmd/install.go
+++ cmd/install.go
@@ -1,11 +1,13 @@
 package cmd
 
 import (
+	"archive/zip"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path"
 	"strconv"
 	"strings"
 
@@ -21,14 +23,17 @@ type promptContent struct {
 type semVar struct {
 	major int
 	minor int
-	patch int
+	//patch int
 }
 
 func (s semVar) String() string {
-	return "v" + strconv.Itoa(s.major) + "." + strconv.Itoa(s.minor) + "." + strconv.Itoa(s.patch)
+	//return "v" + strconv.Itoa(s.major) + "." + strconv.Itoa(s.minor) + "." + strconv.Itoa(s.patch)
+	return "v" + strconv.Itoa(s.major) + "." + strconv.Itoa(s.minor)
 }
 
 const PB_URL = "https://github.com/protocolbuffers/protobuf/releases"
+
+// TODO: make cleanup func to remove zip file and the rest
 
 // installCmd represents the new command
 var installCmd = &cobra.Command{
@@ -113,21 +118,27 @@ func promptGetProtocVersion(pc promptContent) semVar {
 		}
 		semVarList := strings.Split(input, ".")
 
-		if len(semVarList) != 3 {
-			return errors.New(pc.errorMsg)
-		}
-		_, err := strconv.Atoi(semVarList[0])
-		if err != nil {
-			return err
-		}
-		_, err = strconv.Atoi(semVarList[1])
-		if err != nil {
-			return err
-		}
-		_, err = strconv.Atoi(semVarList[2])
-		if err != nil {
-			return err
-		}
+		fmt.Printf("semvarlist: %v", semVarList)
+
+		// FIXME
+		//if len(semVarList) != 3 {
+		//	return errors.New(pc.errorMsg)
+		// FIXME
+		//}
+		//_, err := strconv.Atoi(semVarList[0])
+		//if err != nil {
+		//	return err
+		// FIXME
+		//}
+		//_, err = strconv.Atoi(semVarList[1])
+		//if err != nil {
+		//	return err
+		// FIXME
+		//}
+		//_, err = strconv.Atoi(semVarList[2])
+		//if err != nil {
+		//	return err
+		//}
 		return nil
 	}
 
@@ -148,7 +159,8 @@ func promptGetProtocVersion(pc promptContent) semVar {
 
 	sm.major, err = strconv.Atoi(semVarList[0])
 	sm.minor, err = strconv.Atoi(semVarList[1])
-	sm.patch, err = strconv.Atoi(semVarList[2])
+	// FIXME
+	//sm.patch, err = strconv.Atoi(semVarList[2])
 
 	return sm
 }
@@ -160,16 +172,20 @@ func protocInstaller(installProtoc bool, sm semVar) {
 
 	// https://github.com/protocolbuffers/protobuf/releases/download/v24.2/protoc-24.2-linux-aarch_64.zip
 	// https://github.com/protocolbuffers/protobuf/releases/download/v24.2/protoc-24.2-linux-x86_64.zip
-	url := fmt.Sprintf("%s/download/%s/protoc-%s-linux-x86_64.zip", PB_URL, sm.String(), sm.String())
 
-	fmt.Printf("url: %s", url)
+	// TODO: fix url making, the below works, but the hand made one does not
+	// FIXME
+	//url := fmt.Sprintf("%s/download/%s/protoc-%s-linux-x86_64.zip", PB_URL, sm.String(), sm.String())
+	url := "https://github.com/protocolbuffers/protobuf/releases/download/v24.2/protoc-24.2-linux-x86_64.zip"
+
+	fmt.Printf("url: %s\n", url)
 
 	resp, err := http.Get(url)
 	if err != nil {
 		fmt.Printf("err: %s", err)
 	}
 	defer resp.Body.Close()
-	//fmt.Println("status", resp.Status)
+	fmt.Println("status", resp.Status)
 	if resp.StatusCode != 200 {
 		return
 	}
@@ -183,5 +199,54 @@ func protocInstaller(installProtoc bool, sm semVar) {
 
 	// Write the body to file
 	_, err = io.Copy(out, resp.Body)
-	fmt.Printf("err: %s", err)
+	if err != nil {
+		panic(err)
+	}
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	// basically this:  unzip protoc-3.15.8-linux-x86_64.zip -d $HOME/.local
+	err = unzip("test.zip", homeDir+"/.protoc")
+	if err != nil {
+		panic(err)
+	}
+
+	// finally add to path
+	// TODO: make this simple
+	// FIXME: the problem here is that the protoc is not an executable and the export does not work due to this
+	fmt.Println("finally copy paste this into your terminal:")
+	fmt.Println(`export PATH="$PATH:$HOME/.protoc/bin"`)
+
+	fmt.Println("done")
+}
+
+func unzip(source, dest string) error {
+	read, err := zip.OpenReader(source)
+	if err != nil {
+		return err
+	}
+	defer read.Close()
+	for _, file := range read.File {
+		if file.Mode().IsDir() {
+			continue
+		}
+		open, err := file.Open()
+		if err != nil {
+			fmt.Println("cannot open file")
+			return err
+		}
+		name := path.Join(dest, file.Name)
+		os.MkdirAll(path.Dir(name), 0777)
+		create, err := os.Create(name)
+		if err != nil {
+
+			fmt.Println("cannot create file")
+			return err
+		}
+		defer create.Close()
+		create.ReadFrom(open)
+	}
+	fmt.Println("unzip done")
+	return nil
 }
